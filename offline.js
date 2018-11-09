09cb04e94781c1aac24391502a34b6916882a8e6
artiqle <artiqle@mail.ru>
2018-11-09 12:51:05 +0600

    initial commit

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1541744282,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/her1-sheet0.png",
+		"images/gr1-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tree1-sheet0.png",
+		"images/gr2.png",
+		"images/bg2.png",
+		"images/tiledbackground2.png",
+		"images/her2-sheet0.png",
+		"images/mech1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
